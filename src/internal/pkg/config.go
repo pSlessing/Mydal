579882191ac181814eb641451cf05eb264f00b5e
Peter Slessing <peter@slessing.dk>
2026-05-09 22:32:45 +0200

    Added minio

--- src/internal/pkg/config.go
+++ src/internal/pkg/config.go
@@ -3,17 +3,25 @@ package pkg
 import "os"
 
 type Config struct {
-	Addr        string
-	DatabaseURL string
-	BucketName  string
-	LogLevel    string
+	Addr           string
+	DatabaseURL    string
+	MinioEndpoint  string
+	MinioAccessKey string
+	MinioSecretKey string
+	MinioUseSSL    bool
+	BucketName     string
+	LogLevel       string
 }
 
 func Load() Config {
 	return Config{
-		Addr:        os.Getenv("ADDR"),
-		DatabaseURL: os.Getenv("DATABASE_URL"),
-		BucketName:  os.Getenv("BUCKET_NAME"),
-		LogLevel:    os.Getenv("LOG_LEVEL"),
+		Addr:           os.Getenv("ADDR"),
+		DatabaseURL:    os.Getenv("DATABASE_URL"),
+		MinioEndpoint:  os.Getenv("MINIO_ENDPOINT"),
+		MinioAccessKey: os.Getenv("MINIO_ACCESS_KEY"),
+		MinioSecretKey: os.Getenv("MINIO_SECRET_KEY"),
+		MinioUseSSL:    os.Getenv("MINIO_USE_SSL") == "true",
+		BucketName:     os.Getenv("BUCKET_NAME"),
+		LogLevel:       os.Getenv("LOG_LEVEL"),
 	}
 }
